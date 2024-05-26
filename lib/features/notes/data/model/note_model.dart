aba473293cee325f38b884e6a93e901131fdd0a9
mohamed badr <mahomedbadr05@gmail.com>
2024-05-27 01:03:52 +0300

    create note model

---
+++ lib/features/notes/data/model/note_model.dart
@@ -0,0 +1,14 @@
+class NoteModel {
+
+ final  String title;
+ final String subtitle;
+ final int color;
+ final String date;
+
+  NoteModel({
+    required this.color,
+    required this.title,
+    required this.subtitle,
+    required this.date,
+  });
+}
