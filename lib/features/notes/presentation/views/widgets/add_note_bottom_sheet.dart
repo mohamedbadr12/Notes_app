0ef2902281327101115487fa8a86622ad42af5bb
mohamed badr <mahomedbadr05@gmail.com>
2024-05-27 00:33:21 +0300

    finish add note bottom sheet

--- lib/features/notes/presentation/views/widgets/add_note_bottom_sheet.dart
+++ lib/features/notes/presentation/views/widgets/add_note_bottom_sheet.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:notes_app/features/notes/presentation/views/widgets/custom_text_form_field.dart';
+
+import 'custom_button.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
@@ -8,14 +11,20 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return  const Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(height: 32),
-          CustomTextFormField(hintText: 'Title',),
-          SizedBox(height: 16),
-          CustomTextFormField(hintText: 'Content',maxLines: 5,)
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 32),
+            CustomTextFormField(hintText: 'Title',),
+            SizedBox(height: 16),
+            CustomTextFormField(hintText: 'Content',maxLines: 5,),
+            SizedBox(height: 32),
+            CustomButton(),
+            SizedBox(height: 16),
 
-        ],
+
+          ],
+        ),
       ),
     );
   }
---
+++ lib/features/notes/presentation/views/widgets/custom_button.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      width: MediaQuery.of(context).size.width,
+      height: 55,
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(8),
+
+        color: const Color(0xff62FCD7),
+      ),
+      child: const Center(child: Text('Add',style: TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.bold),),),
+    );
+  }
+}
