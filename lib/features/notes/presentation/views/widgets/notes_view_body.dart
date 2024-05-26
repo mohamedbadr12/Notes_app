d34dbb0f83574b1f57899650b2641e0809c8f631
mohamed badr <mahomedbadr05@gmail.com>
2024-05-26 22:30:12 +0300

    add list view node item

--- lib/features/notes/presentation/views/widgets/notes_view_body.dart
+++ lib/features/notes/presentation/views/widgets/notes_view_body.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'custom_app_bar.dart';
-import 'note_item.dart';
+import 'notes_list_view.dart';
 
 class NotesViewsBody extends StatelessWidget {
   const NotesViewsBody({super.key});
@@ -11,7 +12,7 @@ class NotesViewsBody extends StatelessWidget {
       child: Column(
         children: [
           CustomAppBar(),
-          NoteItem()
+          Expanded(child: NotesListView())
         ],
       ),
     );
